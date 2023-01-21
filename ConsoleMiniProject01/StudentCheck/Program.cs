6666f967e7e01cb1baefa0eb1c8a05d5e79c1f15
Shah Haque <93956445+Shah-Haque@users.noreply.github.com>
2023-01-21 21:21:19 +0000

    Entire Project files
    This is the entire project files for the student checker application as since this is a simple app that checks for three things firstname, lastname and age. I have based this upon the mini project challenge from the Tim Corey C# master course.

---
+++ ConsoleMiniProject01/StudentCheck/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace StudentCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your FirstName: ");
+            string FirstName = Console.ReadLine();
+
+            Console.Write("What is your LastName: ");
+            string LastName = Console.ReadLine();
+
+            Console.Write("How old are you: ");
+            string AgeText = Console.ReadLine();
+
+            string formattedFirstName;
+            string formattedLastName;
+
+            string formattedfullname = FirstName + LastName;
+
+            if (int.TryParse(AgeText, out int age))
+            {
+                if (FirstName.ToLower() == "shah" ||
+                    FirstName.ToLower() == "Tim")
+                {
+                   formattedFirstName = $"Hello Mister {FirstName}";
+                }
+                else
+                {
+                    formattedFirstName= FirstName;
+                }
+                if (LastName.ToLower() == "haque" ||
+                    LastName.ToLower() == "Corey")
+                {
+                    formattedLastName = $"That is a nice {LastName}";
+                }
+                else
+                {
+                    formattedLastName= LastName;
+                }
+                if (age < 21)
+                {
+                    Console.WriteLine($"You should wait {21 - age} years, {formattedfullname}");
+                }
+                else
+                {
+                    Console.WriteLine($"Welcome to class {formattedfullname}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You Fell off");
+            }
+        }
+
+        }
+    }
